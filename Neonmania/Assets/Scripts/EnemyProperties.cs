5a130a19673b60fe030e20af216459439d8e7f88
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2018-06-29 21:58:28 +0200

    moved IKillable, added death handler to enemy

--- Neonmania/Assets/Scripts/EnemyProperties.cs
+++ Neonmania/Assets/Scripts/EnemyProperties.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New EnemyType", menuName = "Enemy Type")]
-public class EnemyProperties : ScriptableObject {
+public class EnemyProperties : ScriptableObject
+{
 
-    [Header("Movement Properties")]
-    [Range(0f, 1f)]
+	[Header("Movement Properties")]
+	[Range(0f, 1f)]
 	public float speed = 2f;
+	public EnemyPathfinding movementLogic;
 
-    [Header("Init Values")]
-    public CMYColor color;
+	[Header("Init Values")]
+	public CMYColor color;
 
-    [Header("Strength")] 
-    [Range(0f, 1f)]
-    public float weakness = 1f;             // higher is weaker
-    [Range(0.3f, 5f)]
-    public float scale = 0.8f;              // higher is greater
-    [Range(0f, 1f)]
-    public float strengthIndicator = 0.1f;    // higher is stronger 
+	[Header("Strength")]
+	[Range(0f, 1f)]
+	// higher is weaker
+	public float weakness = 1f;
+	[Range(0.3f, 5f)]
+	// higher is greater
+	public float scale = 0.8f;
+	[Range(0f, 1f)]
+	// higher is stronger 
+	public float strengthIndicator = 0.1f;
 
-    [Header("Boss Properties")]
-    public bool isBoss = false;             // if true, players
+    [Header("Handlers")]
+    public IKillable deathHandler;
+
+	[Header("Boss Properties")]
+	// if true, players
+	public bool isBoss = false;
 }
