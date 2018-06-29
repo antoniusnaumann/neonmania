c4bc48b39a5cbe4759a8d462e6df6eb20a4b6fee
BlueIceshard <antonius.naumann@student.hpi.uni-potsdam.de>
2018-06-29 20:46:38 +0200

    nice scriptable objects handling

--- Neonmania/Assets/Scripts/EnemyProperties.cs
+++ Neonmania/Assets/Scripts/EnemyProperties.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New EnemyType", menuName = "Enemy Type")]
 public class EnemyProperties : ScriptableObject {
 
+    [Header("Movement Properties")]
 	public float speed = 2f;
-    public TColor color;
-    public float damageLevel = 1f; // higher is weaker
-    public float scale = 0.8f;
-    public bool isBoss = false;
+
+    [Header("Init Values")]
+    public CMYColor color;
+
+    [Header("Strength")] 
+    [Range(0f, 1f)]
+    public float weakness = 1f;             // higher is weaker
+    [Range(0.3f, 5f)]
+    public float scale = 0.8f;              // higher is greater
+    [Range(0f, 1f)]
+    public float strengthIndicator = 0.1f;    // higher is stronger 
+
+    [Header("Boss Properties")]
+    public bool isBoss = false;             // if true, players
 }
