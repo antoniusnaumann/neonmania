9f513f100d59126345c62bd5eaab0e1d160a188d
Antonius Naumann <antonius.naumann@t-online.de>
2018-06-29 20:43:42 +0200

    PlayerColorChanger added color change

--- Neonmania/Assets/Scripts/PlayerColorChanger.cs
+++ Neonmania/Assets/Scripts/PlayerColorChanger.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorChanger : MonoBehaviour {
+public class PlayerColorChanger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+    private Rigidbody rb;
+    private int color;
+
+    private void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		
 	}
+
+    private void SwitchColor() {
+        color = (color + 1) % 3;
+    }
+
+    private Color GetColor() {
+        switch (color) {
+            case 0:
+                return CMYColor.CYAN.AsColor();
+            case 1:
+                return CMYColor.MAGENTA.AsColor();
+            case 2:
+                return CMYColor.YELLOW.AsColor();
+            default:
+                throw new ArgumentException("Invalid Color Value");
+        }
+    }
 }
