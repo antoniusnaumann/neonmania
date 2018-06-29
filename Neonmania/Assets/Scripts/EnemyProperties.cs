d2a4a94ac68e088cadf1dbd308b5fb62dd0e7d88
BlueIceshard <antonius.naumann@student.hpi.uni-potsdam.de>
2018-06-29 20:31:55 +0200

    scriptable object folder structure

---
+++ Neonmania/Assets/Scripts/EnemyProperties.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New EnemyType", menuName = "Enemy Type")]
+public class EnemyProperties : ScriptableObject {
+
+	public float speed = 2f;
+    public TColor color;
+    public float damageLevel = 1f; // higher is weaker
+    public float scale = 0.8f;
+    public bool isBoss = false;
+}
