eee113edc231e5d03dbee636cfc15f03e16b1d71
jasper.blum <jasper.blum@student.hpi.uni-potsdam.de>
2018-06-29 22:00:14 +0200

    Added Controller Input, trying to get stuff to shoot

--- Neonmania/Assets/Scripts/PlayerControl.cs
+++ Neonmania/Assets/Scripts/PlayerControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
     private Rigidbody rb;
-
+    GameObject projectilePrefab;
 
     public float acceleration = 10f;
     public float maxSpeed = 30f;
@@ -14,23 +14,21 @@ public class PlayerControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-    }
+        projectilePrefab = Resources.Load("Projectile") as GameObject;    }
 
     // Before performing physics calculation
     void FixedUpdate() {
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("LeftJoystickHorizontal");
+        float moveVertical = Input.GetAxis("LeftJoystickVertical");
 
         Vector3 v3 = new Vector3(moveHorizontal * acceleration, 0f, moveVertical * acceleration);
 
-
         if (moveHorizontal==0 && moveVertical==0)
         {
-            //The player is not pressing any buttons, adding the mirrored vector to slow down
+            //The player is not moving LeftJoystick any buttons, adding the mirrored vector to slow down
             Vector3 mirrorDirection = rb.velocity * (-1);
             rb.AddForce(mirrorDirection.normalized * deceleration);
-            
         }
         else
         {
@@ -44,15 +42,20 @@ public class PlayerControl : MonoBehaviour {
             }
         }
 
-
         
-        
-        
-
     }
 
     // Update is called once per frame
     void Update () {
+        float shootHorizontal = Input.GetAxis("RightJoystickHorizontal");
+        float shootVertical = Input.GetAxis("RightJoystickHorizontal");        
+
+        if (shootHorizontal != 0 || shootHorizontal != 0) {
+            if(Input.GetAxis("AButton") != 0){
+                Vector3 aimVector = new Vector3(shootHorizontal, 10f, shootVertical);
+                GameObject projectile = Instantiate(projectilePrefab, transform) as GameObject;
+            }
+        }
 		
 	}
 }
