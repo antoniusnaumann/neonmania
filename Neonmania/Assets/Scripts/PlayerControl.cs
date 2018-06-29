7651a60cfd4e5f7d604c690c220b970af278d0ab
Antonius Naumann <antonius.naumann@student.hpi.uni-potsdam.de>
2018-06-29 17:10:52 +0200

    Bewegung hinzugefuegt

---
+++ Neonmania/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Before performing physics calculation
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 v3 = new Vector3(moveHorizontal * speed, 0f, moveVertical * speed);
+
+        rb.AddForce(v3);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
