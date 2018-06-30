07e4d8380f76cb870650748ab8be3dcc25a6aac2
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2018-06-30 17:33:21 +0200

    added disco lights

---
+++ Neonmania/Assets/Scripts/DiscoLightScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoLightScript : MonoBehaviour {
+
+	private Light Light;
+
+	public float hueLoopPeriodInS;
+
+	private void Start() {
+		this.Light = GetComponent<Light>();
+	}
+
+	private void Update() {
+		// UpdateLightHue((Time.time % hueLoopPeriodInS) / hueLoopPeriodInS);
+		UpdateLightBrightness(Math.Abs((Time.time % 2) - 1));
+	}
+
+	private void UpdateLightHue(float hue) {
+		float h, s, v;
+		Color.RGBToHSV(this.Light.color, out h, out s, out v);
+		this.Light.color = Color.HSVToRGB(hue, s, v);
+	}
+
+	private void UpdateLightBrightness(float brightness) {
+		float h, s, v;
+		Color.RGBToHSV(this.Light.color, out h, out s, out v);
+		this.Light.color = Color.HSVToRGB(h, s, brightness);
+	}
+}
