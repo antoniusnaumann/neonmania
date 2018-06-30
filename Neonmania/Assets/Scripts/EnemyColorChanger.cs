ae723867c0417a9682b7032f4f06ef5510d23748
Antonius Naumann <antonius.naumann@t-online.de>
2018-06-30 11:52:54 +0200

    Set random color on spawn enemy

--- Neonmania/Assets/Scripts/EnemyColorChanger.cs
+++ Neonmania/Assets/Scripts/EnemyColorChanger.cs
@@ -1,8 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyColorChanger : MonoBehaviour {
+
+    private readonly CMYColor[] level1Colors = {
+        CMYColor.CYAN,
+        CMYColor.MAGENTA,
+        CMYColor.YELLOW
+    };
+
+    private readonly CMYColor[] level2Colors = {
+        CMYColor.RED,
+        CMYColor.GREEN,
+        CMYColor.BLUE
+    };
 
     private EnemyProperties enemy;
     private EnemyPathfinding killHandler;
@@ -12,7 +25,9 @@ public class EnemyColorChanger : MonoBehaviour {
 	private void Start () {
         enemy = GetComponent<EnemyPropertyController>().properties;
         killHandler = GetComponent<EnemyPathfinding>();
-        // currentColor = GetComponent<EnemyPropertyController>().color;
+        int level = GetComponent<EnemyProperties>().level;
+
+        currentColor = GetLevelColor(level);
 
         ApplyColor(currentColor);
 	}
@@ -30,6 +45,21 @@ public class EnemyColorChanger : MonoBehaviour {
             if (currentColor.IsBlack()) {
                 killHandler.OnDeath();
             }
+        }
+    }
+
+    private CMYColor GetLevelColor(int level) {
+        int colorValue = UnityEngine.Random.Range(0, 3);
+        switch (level) {
+            case 0:
+                return level1Colors[colorValue];
+            case 1:
+                return level2Colors[colorValue];
+            case 2:
+                return CMYColor.WHITE;
+            default:
+                throw new ArgumentException(
+                    string.Format("Level over {0} not implemented yet", level));
         }
     }
 
